56fb75b0ae103f16d0924ab09510aeac80de9d6e
Max <109218111+fyflo@users.noreply.github.com>
2025-02-12 05:01:39 -0800

    Removed white background when overlay is launched

--- HUD/public/js/list.js
+++ HUD/public/js/list.js
@@ -1,13 +1,14 @@
 $(document).ready(() => {
-	function listener(data) {
-        let instances = data.instances;
-        instances.forEach(function(hud) {
-            let $new = $("#example").clone().removeClass("#example");
-            $new.find("p").html("<b>"+hud.hud + " </b> - " + hud.name);
-            $new.find("a").attr("href", "/huds/"+hud._id);
-            $new.appendTo("#list");
-        }, this);
-        $("#example").remove();
-	}
-    loadHUDs(listener);
+  $("body").css("background-color", "rgba(0, 0, 0, 0)");
+  function listener(data) {
+    let instances = data.instances;
+    instances.forEach(function (hud) {
+      let $new = $("#example").clone().removeClass("#example");
+      $new.find("p").html("<b>" + hud.hud + " </b> - " + hud.name);
+      $new.find("a").attr("href", "/huds/" + hud._id);
+      $new.appendTo("#list");
+    }, this);
+    $("#example").remove();
+  }
+  loadHUDs(listener);
 });
